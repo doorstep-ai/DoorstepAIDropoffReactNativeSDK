c30d54a22b25f13ef3b3c89e41ddccdb94a5e67a
Sheel Patel <sheel@doorstep.ai>
2025-04-15 15:30:27 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# @doorstepai/dropoff-sdk
+
+ 
+
+## Installation
+
+```sh
+npm install @doorstepai/dropoff-sdk
+```
+
+## Usage
+
+
+```js
+import { multiply } from '@doorstepai/dropoff-sdk';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/doorstepai/dropoffsdk/DropoffSdkPackage.kt
@@ -0,0 +1,17 @@
+package com.doorstepai.dropoffsdk
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class DropoffSdkPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(DropoffSdkModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
